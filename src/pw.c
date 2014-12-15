ac2696498432fa5075b1b67a49ccbc9218c5ed59
CubeLin <marco.l.here@gmail.com>
2014-12-15 18:53:58 +0800

    make use of do{...} while(0) construct

--- src/pw.c
+++ src/pw.c
@@ -1,5 +1,4 @@
 /**
- *
  * Printf wrapper
  */
 #include <stdio.h>
@@ -7,52 +6,44 @@
 #define COL             "\033[0m"
 #define COL_BRED        "\033[1;31m"
 #define COL_BGREEN      "\033[1;32m"
-#define COL_BBROWN     "\033[2;33m"
+#define COL_BBROWN      "\033[2;33m"
 #define COL_BBLUE       "\033[1;34m"
 #define COL_BPURPLE     "\033[1;35m"
 #define COL_BCYAN       "\033[1;36m"
 #define COL_BWHITE      "\033[1;37m"
 
 /**
- *
  * Debug
  */
 #ifdef DEBUG
-        #define dprintf(format, args...) \
-        { \
+        #define dprintf(format, args...) do { \
                 printf(COL_BBLUE "[DEBUG] " COL); \
                 printf(format, ## args); \
-        }
+        } while (0)
 #else
         #define dprintf(format, args...)
 #endif
 
 /**
- *
  * Information
  */
-#define iprintf(format, args...) \
-{ \
-        printf(COL_BGREEN "[INFO] " COL ); \
-        printf(format, ## args); \
-}
+#define iprintf(format, args...) do { \
+            printf(COL_BGREEN "[INFO] " COL ); \
+            printf(format, ## args); \
+        } while (0)
 
 /**
- *
  * Warning
  */
-#define wprintf(format, args...) \
-{ \
-        printf(COL_BBROWN "[WARNING] " COL); \
-        printf(format, ## args); \
-}
+#define wprintf(format, args...) do { \
+            printf(COL_BBROWN "[WARNING] " COL); \
+            printf(format, ## args); \
+        } while (0)
 
 /**
- *
  * Error
  */
-#define eprintf(format, args...) \
-{ \
-        fprintf(stderr, COL_BPURPLE "[ERROR] " COL); \
-        fprintf(stderr, format, ## args); \
-}
+#define eprintf(format, args...) do { \
+            fprintf(stderr, COL_BPURPLE "[ERROR] " COL); \
+            fprintf(stderr, format, ## args); \
+        } while (0)
